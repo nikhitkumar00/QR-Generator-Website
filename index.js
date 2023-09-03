0fd6c53e31ebdadf4640d7727e454faf056c1a15
nikhitkumar00 <nikhitkumar00@gmail.com>
2023-09-03 20:51:53 +0530

    added js

---
+++ index.js
@@ -0,0 +1,6 @@
+const slider = document.getElementById("quality");
+const sliderValue = document.getElementById("qualityValue");
+
+slider.addEventListener("input", () => {
+	sliderValue.textContent = slider.value;
+});
